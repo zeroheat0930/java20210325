701b2dc7b361b549e4851407b1aac0619d7b7dd0
zeroheat0930 <dongjunkorea@naver.com>
2021-04-02 10:15:14 +0900

    MASTER

---
+++ src/p05/lecture/A02ReferenceType.java
@@ -0,0 +1,21 @@
+package p05.lecture;
+
+public class A02ReferenceType {
+	// 그림 : 01.참조타입.png
+	public static void main(String[] args) {
+		int i;
+		i = 3;
+		
+		String s;
+		s = new String("java");
+		
+		// String : 타입
+		// s : (참조)변수
+		// new String("java") : 객체(instance(개체), object(객체))
+		// s 가 가진 값: 참조값
+		
+	}
+}
+
+
+
